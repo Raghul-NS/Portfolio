a3bca9204827a806f4af91bdc92ed5b70017a26d
Raghul N.S <raghulpravee799@gmail.com>
2026-07-14 13:55:53 +0530

    Fix the scrolling behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Works from './Pages/Works';
@@ -8,6 +9,7 @@ import LetsTalk from "./Pages/Let'sTalk";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
